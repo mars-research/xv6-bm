250d68e93114ac574ea49bf6ebc438a8a497378a
Anton Burtsev <aburtsev@flux.utah.edu>
2018-04-21 15:19:37 -0700

    Not sure why startothers was commented out

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ main(void)
   binit();         // buffer cache
   fileinit();      // file table
   ideinit();       // disk 
-  //startothers();   // start other processors
+  startothers();   // start other processors
   kinit2(P2V(4*1024*1024), P2V(PHYSTOP)); // must come after startothers()
   userinit();      // first user process
   mpmain();        // finish this processor's setup
